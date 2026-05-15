f78344c73376148024372b394de40285f01fff66
joeshpmorepha <lchd.football@outlook.com>
2026-05-15 14:17:02 +0530

    Update proxyurl.js

--- proxyurl.js
+++ proxyurl.js
@@ -1,9 +1,2 @@
-const PROXY_CONFIG = {
-
-proxy: "https://fncd.kuvuslov.cymru/",
-
-// true = use proxy even in India
-// false = proxy only outside India
-indiaMode: false
-
-};
+const PROXY_URL =
+"https://fncd.kuvuslov.cymru/";
